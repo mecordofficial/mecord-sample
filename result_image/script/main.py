059d2eeccf4795e84f1bfd2789a227712404f503
DESKTOP-030C45B\123 <pengjun@xinyu668.com>
2023-03-13 19:33:46 +0800

    update sample

--- result_image/script/main.py
+++ result_image/script/main.py
@@ -1,18 +1,13 @@
+##################################################
+# do not change this file, real File is [run.py] #
+##################################################
 import argparse
 import subprocess
 import os
 from run import *
 
 parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument("--run", type=str, default=None, help=("""请求参数, JSON格式如下: 
-    {
-        \"task_id\": [task_id],
-        \"widget_id\": [widget_id],
-        \"pending_count\": [pending_count],
-        \"fn_name\": \"test\",
-        \"param\": {"
-            \"text\":\"Hello World\"
-        }"""))
+parser.add_argument("--run", type=str, default=None, help=("input param file path"))
 parser.add_argument("--out", type=str, default=None, help=("output file path"))
 cmd_opts = parser.parse_args()
 
